b88b5cf57a98c09b6cfc25beb2f8003d22bd2f7d
Johnman747 <36646172+Johnman747@users.noreply.github.com>
2018-08-22 15:10:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+function moveHands(){
+  with (new date()){
+    h = 30 * ((getHours() % 12) + getMinuets() / 60)
+    m = 6 * getMinutes()
+    s = 6 * getSeconds()
+    document.getElementById("seconds").style.cssText =
+      "-webkit-transform:rotate(" + s + "deg);";
+    document.getElementById("minutes").style.cssText =
+      "-webkit-transform:rotate(" + m + "deg);";
+    document.getElementById("hours").style.cssText =
+      "-webkit-transform:rotate(" + h + "deg);";
 
+    setTimeout(moveHands, 1000); 
+  }
+}
